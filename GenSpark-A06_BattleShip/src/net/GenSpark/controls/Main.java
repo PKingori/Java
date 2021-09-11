ccdcf8d81eb728374c33c13cdfb9b7740a36de78
Patrick Kingori <pat_kingori@yahoo.com>
2021-09-11 01:24:20 -0500

    Code refactor

--- GenSpark-A06_BattleShip/src/net/GenSpark/controls/Main.java
+++ GenSpark-A06_BattleShip/src/net/GenSpark/controls/Main.java
@@ -16,7 +16,7 @@ package net.GenSpark.controls;
 // **************************** MAIN SWITCH *****************************
 public class Main {
     public static void main(String[] args) {
-        Ignition battle = new Ignition();
+        var battle = new Ignition();
         battle.start();
         }
     /* ******************************************************************* */
